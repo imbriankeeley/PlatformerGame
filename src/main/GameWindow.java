216c51964ac8177f7d6fc612bc4300c66f986ba0
Brian Keeley <imbriankeeley@gmail.com>
2024-05-05 03:50:22 -0400

    Created panel to draw for program.

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,15 +3,15 @@ package main;
 import javax.swing.*;
 
 public class GameWindow {
-    private JFrame jframe;
+    private JFrame frame;
 
     public GameWindow() {
 
-        jframe = new JFrame();
+        frame = new JFrame();
 
-        jframe.setSize(800, 800);
-        jframe.setVisible(true);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setLocationRelativeTo(null);
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
